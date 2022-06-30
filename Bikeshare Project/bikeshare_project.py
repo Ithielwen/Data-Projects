7d8cfcaa0330ec7d90e05a2d30e31be47a6cf496
Emma Hungrige <103974683+Ithielwen@users.noreply.github.com>
2022-06-29 22:16:50 -0700

    def get_filter comments

--- Bikeshare Project/bikeshare_project.py
+++ Bikeshare Project/bikeshare_project.py
@@ -18,4 +18,13 @@ city_data = { 'Chicago': 'chicago.csv', 'New York City': 'new_york_city.csv', 'W
 
 #Definitions
 
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
 
+    Returns:
+        city (str) - name of the city to analyze
+        month (str) - name of the month to filter by, or "all" to apply no month filter
+        day (str) - name of the day of the week to filter by, or "all" to apply no day filter
+    """
+    
