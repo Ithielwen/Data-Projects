ede7345e197cf85bfb00ce14474a8d5b67569f57
Emma Hungrige <103974683+Ithielwen@users.noreply.github.com>
2022-06-29 22:14:25 -0700

    csv variable

--- Bikeshare Project/bikeshare_project.py
+++ Bikeshare Project/bikeshare_project.py
@@ -7,7 +7,15 @@ Student: Emma Hungrige
 """
 
 #Import Section
+
 import time
 import pandas as pd
 import numpy as np
+
+#CSV Variable
+
+city_data = { 'Chicago': 'chicago.csv', 'New York City': 'new_york_city.csv', 'Washington': 'washington.csv'}
+
+#Definitions
+
 
